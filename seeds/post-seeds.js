9826abef96ac7d2041ff999f94a45606717fdbfc
Noah <nosherway@yahoo.com>
2021-04-19 20:38:00 -0500

    adding repaired files

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,107 +1,114 @@
-const { Post } = require('../models');
+const { Post } = require("../models");
 
 const postdata = [
   {
-    title: 'Donec posuere metus vitae ipsum.',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
-    user_id: 10
+    title: "Milk is good for technology",
+    post_url:
+      "https://www.watchusgrow.org/2015/07/20/20-things-you-didnt-know-you-wanted-to-know-about-milk/",
+    user_id: 10,
   },
   {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'https://nasa.gov/donec.json',
-    user_id: 8
+    title: "Mars Helicopter developed by NASA completes first mission",
+    post_url:
+      "https://www.wsj.com/articles/nasas-ingenuity-helicopter-successfully-makes-historic-first-flight-on-mars-11618830461?mod=tech_lead_pos5",
+    user_id: 8,
   },
   {
-    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
-    user_id: 1
+    title: "Vin diesel is actually a robot disguised as a human",
+    post_url: "https://www.factinate.com/people/43-fast-facts-vin-diesel/",
+    user_id: 1,
   },
   {
-    title: 'Nunc purus.',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
-    user_id: 4
+    title: "A new sub-genre of Tech House emerges: Bohemian Metal House",
+    post_url: "http://www.slothville.com/",
+    user_id: 4,
   },
   {
-    title: 'Pellentesque eget nunc.',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
-    user_id: 7
+    title: "Bob Saget invents first mechanical paper airplane",
+    post_url:
+      "https://www.deviantart.com/urkel8534/art/Bob-Saget-Card-107306435",
+    user_id: 7,
   },
   {
-    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    post_url: 'https://stanford.edu/consequat.png',
-    user_id: 4
+    title: "Microchip that helps chickens plant eggs faster to release May 1st",
+    post_url:
+      "https://www.mentalfloss.com/article/89007/brief-history-chicken-dance",
+    user_id: 4,
   },
   {
-    title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
-    user_id: 1
-  },
-  {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
-    user_id: 1
-  },
-  {
-    title: 'Duis ac nibh.',
-    post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
-    user_id: 9
-  },
-  {
-    title: 'Curabitur at ipsum ac tellus semper interdum.',
-    post_url: 'https://reverbnation.com/ligula/sit.jpg',
-    user_id: 5
-  },
-  {
-    title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://china.com.cn/lectus/vestibulum.json',
-    user_id: 3
-  },
-  {
-    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
-    user_id: 10
-  },
-  {
-    title: 'Donec dapibus.',
-    post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
-    user_id: 8
-  },
-  {
-    title: 'Nulla tellus.',
-    post_url: 'https://lycos.com/natoque/penatibus/et.html',
-    user_id: 3
-  },
-  {
-    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-    post_url: 'https://gmpg.org/lorem.jpg',
-    user_id: 3
+    title: "50 cent was Jake Paul's Robot",
+    post_url: "https://www.reddit.com/r/funny/comments/iqren/50_centaur/",
+    user_id: 1,
   },
   {
     title:
-      'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-    post_url: 'https://paginegialle.it/mattis/egestas.jsp',
-    user_id: 7
+      "Javascript creater Brendan Eich builds first invisible cloak in 9.5 days",
+    post_url: "https://kids.scholastic.com/kids/books/harry-potter/",
+    user_id: 1,
   },
   {
-    title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://wikia.com/turpis/eget.jpg',
-    user_id: 6
+    title: "Apple to present first macbook boat",
+    post_url:
+      "https://www.fundealer.com/default.asp?page=inventory&subcategory=pontoon",
+    user_id: 9,
   },
   {
-    title: 'Etiam justo.',
-    post_url: 'https://shareasale.com/quis.json',
-    user_id: 4
+    title: "Curabitur at ipsum ac tellus semper interdum.",
+    post_url: "https://reverbnation.com/ligula/sit.jpg",
+    user_id: 5,
   },
   {
-    title: 'Nulla ut erat id mauris vulputate elementum.',
-    post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
-    user_id: 6
+    title: "Hello",
+    post_url: "http://china.com.cn/lectus/vestibulum.json",
+    user_id: 3,
   },
   {
-    title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    post_url: 'https://java.com/at/nibh/in.png',
-    user_id: 7
-  }
+    title: "Hope",
+    post_url:
+      "http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json",
+    user_id: 10,
+  },
+  {
+    title: "You",
+    post_url: "https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml",
+    user_id: 8,
+  },
+  {
+    title: "Are",
+    post_url: "https://lycos.com/natoque/penatibus/et.html",
+    user_id: 3,
+  },
+  {
+    title: "Enjoying",
+    post_url: "https://gmpg.org/lorem.jpg",
+    user_id: 3,
+  },
+  {
+    title: "This",
+    post_url: "https://paginegialle.it/mattis/egestas.jsp",
+    user_id: 7,
+  },
+  {
+    title: "Not",
+    post_url: "http://wikia.com/turpis/eget.jpg",
+    user_id: 6,
+  },
+  {
+    title: "So",
+    post_url: "https://shareasale.com/quis.json",
+    user_id: 4,
+  },
+  {
+    title: "Boring",
+    post_url:
+      "http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png",
+    user_id: 6,
+  },
+  {
+    title: "BUTFUNBLOGGGG",
+    post_url: "https://java.com/at/nibh/in.png",
+    user_id: 7,
+  },
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
